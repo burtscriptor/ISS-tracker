712d74de22f448fcca97bfadc8af5c57b7279f03
David Burt <burt.david.j@gmail.com>
2024-05-15 14:14:57 +1000

    removed empty index.css, new build

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { ChakraProvider, theme } from '@chakra-ui/react'
-
-
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
